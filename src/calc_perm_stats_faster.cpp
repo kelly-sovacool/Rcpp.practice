d396ad39356bf4fc411e0fda6b95b067fe11d448
Kelly Sovacool <kellysovacool@gmail.com>
2021-12-01 14:24:39 -0500

    calc_pvalue performs just as well regardless
    Virtually no difference between R with sum & length, cpp with countif,
    and cpp with an iterator.

--- src/calc_perm_stats_faster.cpp
+++ src/calc_perm_stats_faster.cpp
@@ -32,3 +32,48 @@ test_stat <- sample(perm_perfs, 1)
 calc_pvalue_R(perm_perfs, test_stat)
 calc_pvalue_cpp(perm_perfs, test_stat)
 */
+
+// [[Rcpp::export]]
+double calc_pvalue_cpp_for(NumericVector perm_perfs, double test_stat) {
+    int counts = 0;
+    NumericVector::iterator it;
+    for(it = perm_perfs.begin(); it != perm_perfs.end(); ++it) {
+        if(*it > test_stat) {
+            counts++;
+        }
+    }
+    double pvalue = (double)counts / perm_perfs.size();
+    return pvalue;
+}
+/*** R
+
+perm_perfs <- rnorm(1000)
+test_stat <- sample(perm_perfs, 1)
+calc_pvalue_R(perm_perfs, test_stat)
+calc_pvalue_cpp_for(perm_perfs, test_stat)
+
+
+pval_cpp_countif <- function() {
+    lapply(seq.int(100), function() {
+        perm_perfs <- rnorm(1000)
+        test_stat <- sample(perm_perfs, 1)
+        calc_pvalue_cpp(perm_perfs, test_stat)
+    })
+}
+pval_cpp_for <- function() {
+    lapply(seq.int(100), function() {
+        perm_perfs <- rnorm(1000)
+        test_stat <- sample(perm_perfs, 1)
+        calc_pvalue_cpp_for(perm_perfs, test_stat)
+    })
+}
+pval_R <- function() {
+    lapply(seq.int(100), function() {
+        perm_perfs <- rnorm(1000)
+        test_stat <- sample(perm_perfs, 1)
+        calc_pvalue_R(perm_perfs, test_stat)
+    })
+}
+microbenchmark::microbenchmark(pval_R, pval_cpp, pval_cpp_for, times = 100000L)
+
+*/
